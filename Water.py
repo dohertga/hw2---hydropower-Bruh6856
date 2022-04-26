15606cd3ccdd5b8a869bcdd11dab35ad50078e8b
zach <lehmanza@oregonstate.edu>
2022-04-25 21:09:03 -0700

    Feature Complete

---
+++ Water.py
@@ -0,0 +1,49 @@
+import pandas as pa
+
+spreadsheet = pa.read_csv('energy_use_terrawatthours.csv')
+
+year1 = input("year 1 ")
+year2 = input("year 2 ")
+powerType = input("power type ")
+
+year1PowList = []
+year2PowList = []
+for i in range(len(spreadsheet)):
+    if  str(spreadsheet.at[i, 'year']) == str(year1):
+        year1PowList.append((spreadsheet.at[i, 'generation_twh'], spreadsheet.at[i, 'variable']))
+    if str(spreadsheet.at[i, 'year']) == str(year2):
+        year2PowList.append((spreadsheet.at[i, 'generation_twh'], spreadsheet.at[i, 'variable']))
+year1PowTypeList = []
+year2PowTypeList = []
+for i in range(len(year1PowList)):
+    if str(year1PowList[i][1]) == str(powerType):
+        year1PowTypeList.append(year1PowList[i][0])
+for i in range(len(year2PowList)):
+    if str(year2PowList[i][1]) == str(powerType):
+        year2PowTypeList.append(year2PowList[i][0])
+
+year1PowTypeSum = 0
+year2PowTypeSum = 0
+
+for i in range(len(year1PowTypeList)):
+    year1PowTypeSum += year1PowTypeList[i]
+for i in range(len(year2PowTypeList)):
+    year2PowTypeSum += year2PowTypeList[i]
+
+
+year1PowSum = 0
+year2PowSum = 0
+for i in range(len(year1PowList)):
+    year1PowSum += year1PowList[i][0]
+for i in range(len(year2PowList)):
+    year2PowSum += year2PowList[i][0]
+perInc = round(((year1PowSum/year2PowSum) * 100), 2)
+yearDiff = int(year2) - int(year1)
+perTypeYear1 = round(((year1PowTypeSum/year1PowSum) * 100), 2)
+perTypeYear2 = round(((year2PowTypeSum/year2PowSum) * 100), 2)
+print("The total world power consumption in", str(year1), " was", str(year1PowSum),
+      " terawatt-hours and in", str(year2), "the consumption has increased to",
+      str(year2PowSum), "terawatt-hours.", "That means world power generation has increased by"
+      , str(perInc), "over the past", str(yearDiff), "years.\n", "In year", str(year1),
+      str(perTypeYear1), "% of world energy came from", str(powerType), ", and in", str(year2),
+      str(perTypeYear2), "% of world energy came from", str(powerType), ".")
